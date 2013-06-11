0d1f23745f1cd81d338fe8ca0e14bb265d386a9e
c <jaiquipa300972@gmail.com>
2013-06-10 20:20:44 -0500

    Iniciando proyecto

---
+++ PC02-Caballos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PC02Caballos::Application.config.secret_token = '31099c9b5dea55a79a14ce7f82bdcbcad270af8e56cdf342668d5e35bfa37575718b266e0311364ec1bc97b833d7d94447624848f3a96c4393e01fbef0be5bfd'
