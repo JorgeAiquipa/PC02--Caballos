a7a179126cd855a30b015a80752a422a53584569
c <jaiquipa300972@gmail.com>
2013-06-10 22:02:10 -0500

    Enlazando facebook con la plantilla

---
+++ PC02-Caballos/app/controllers/calculatewins_controller.rb
@@ -0,0 +1,26 @@
+class CalculatewinsController < ApplicationController
+  # GET /horses
+  # GET /horses.json
+  def calculatewin
+    horserace = params[:horserace]
+    bettype = params[:bettype]
+    horse1 = params[:id1]
+    chk1 = params[:chk1]
+    horse2 = params[:id2]
+    chk2 = params[:chk2]
+    horse3 = params[:id3]
+    chk3 = params[:chk3]
+    horse4 = params[:id4]
+    chk4 = params[:chk4]
+    horse5 = params[:id5]
+    chk5 = params[:chk5]
+    horse6 = params[:id6]
+    chk6 = params[:chk6]
+
+    if 
+
+
+
+  end
+
+end
